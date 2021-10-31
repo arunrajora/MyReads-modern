23099ca9c16faf186b465978f64f4a1ed347306f
Arun Rajora <rajora.rajora2@gmail.com>
2021-10-31 19:49:39 +0530

    Break UI into components.

---
+++ src/Home.js
@@ -0,0 +1,64 @@
+import Book from './Book';
+
+function Home(props) {
+  return (
+    <div className='list-books'>
+      <div className='list-books-title'>
+        <h1>MyReads</h1>
+      </div>
+      <div className='list-books-content'>
+        <div>
+          <div className='bookshelf'>
+            <h2 className='bookshelf-title'>Currently Reading</h2>
+            <div className='bookshelf-books'>
+              <ol className='books-grid'>
+                <li>
+                  <Book />
+                </li>
+                <li>
+                  <Book />
+                </li>
+              </ol>
+            </div>
+          </div>
+          <div className='bookshelf'>
+            <h2 className='bookshelf-title'>Want to Read</h2>
+            <div className='bookshelf-books'>
+              <ol className='books-grid'>
+                <li>
+                  <Book />
+                </li>
+                <li>
+                  <Book />
+                </li>
+              </ol>
+            </div>
+          </div>
+          <div className='bookshelf'>
+            <h2 className='bookshelf-title'>Read</h2>
+            <div className='bookshelf-books'>
+              <ol className='books-grid'>
+                <li>
+                  <Book />
+                </li>
+                <li>
+                  <Book />
+                </li>
+                <li>
+                  <Book />
+                </li>
+              </ol>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='open-search'>
+        <button onClick={() => this.setState({ showSearchPage: true })}>
+          Add a book
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
